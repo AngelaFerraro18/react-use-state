5b2977889906b7a72eae3a6085c7e170caba10a6
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-08 15:01:38 +0200

    feat: import languages.js and import ButtonList

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+//importo il componente ButtonList
+import ButtonList from "./components/ButtonList"
+//importo il modulo contenente l'array di oggetti
+import languages from "./languages"
+
 
 function App() {
 
-
   return (
     <>
-
+      <ButtonList languages={languages} />
     </>
   )
 }
