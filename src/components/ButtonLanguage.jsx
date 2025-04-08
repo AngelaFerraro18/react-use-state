e5f16efd9116eaf044d97eebcc6f2f655b0b30dc
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-08 17:55:48 +0200

    feat: create useState to start with the first card open and a generic message, then onClick it will change description

--- src/components/ButtonLanguage.jsx
+++ src/components/ButtonLanguage.jsx
@@ -3,11 +3,14 @@
 import CardDescription from "./CardDescription";
 
 //creo il componente ButtonLanguage e come props vado a destrutturare per prendere direttamente gli elementi title e description, aggiungo le props create in ButtonList per tenere traccia della card aperta
-function ButtonLanguage({ title, description, isOpen, onToggle }) {
+function ButtonLanguage({ title, description, isOpen, onToggle, changeText }) {
 
     return (<div>
         {/* vado ad assegnare all'evento onClick la funzione setIsActive dove vado a cambiare lo stato iniziale dell'elemento false -> true */}
-        <button onClick={onToggle}>{title}</button>
+        <button onClick={() => {
+            onToggle(),
+                changeText();
+        }}>{title}</button>
 
         {/* di conseguenza, se lo stato iniziale è true, allora vedrò il p, altrimenti no */}
         {isOpen && <CardDescription title={title} description={description} />}
--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -6,11 +6,22 @@ import ButtonLanguage from "./ButtonLanguage";
 //creo il componente ButtonList e metto come props l'attributo languages e uso il metodo map per ricreare in pagina la lista degli elementi presenti nell'array fornito
 function ButtonList({ languages }) {
 
-    const [cardOpen, isCardOpen] = useState(null);
+    //imposto come stato iniziale la prima card, che sarà aperta
+    const [cardOpen, setCardOpen] = useState(1);
+    //imposto come stato iniziale un messaggio generico nella card
+    const [activeDescription, setActiveDescription] = useState('Nessun linguaggio selezionato');
+
 
     return <div>
         {languages.map(element =>
-            <ButtonLanguage key={element.id} title={element.title} description={element.description} isOpen={cardOpen === element.id} onToggle={() => isCardOpen(element.id)} />
+            <ButtonLanguage key={element.id}
+                title={element.title}
+                //verifico su quale elemento ho cliccato, di conseguenza vedrò o il messaggio generico, altrimenti vedrò il testo in description
+                description={cardOpen === element.id ? activeDescription : element.description}
+                isOpen={cardOpen === element.id}
+                onToggle={() => setCardOpen(cardOpen === element.id ? 1 : element.id)}
+                changeText={() => setActiveDescription(prev => prev === element.description ? 'Nessun linguaggio selezionato' : element.description)
+                } />
         )}
     </div>
 }
