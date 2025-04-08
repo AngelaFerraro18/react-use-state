aedfa06c867686e147af00b968f98cec1189d901
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-08 16:02:26 +0200

    feat: create CardDescription component

---
+++ src/components/CardDescription.jsx
@@ -0,0 +1,7 @@
+//creo il componente per la card con la descrizione: CardDescription
+function CardDescription({ title, description }) {
+    return <div><h2>{title}</h2><p>{description}</p></div>
+}
+
+//esporto
+export default CardDescription;
