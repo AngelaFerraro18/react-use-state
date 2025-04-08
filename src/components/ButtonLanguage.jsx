b0b6b940d3c1c334c10307c9e45ae91ebbad2be2
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-08 15:39:55 +0200

    feat: import useState and add event onClick to the btn

--- src/components/ButtonLanguage.jsx
+++ src/components/ButtonLanguage.jsx
@@ -1,8 +1,18 @@
+//importo useState da react
+import { useState } from "react";
+
 //creo il componente ButtonLanguage e come props vado a destrutturare per prendere direttamente gli elementi title e description
 function ButtonLanguage({ title, description }) {
+
+    //destrutturo e assegno dei nomi, dopodichè imposto lo stato iniziale a false
+    const [isActive, setIsActive] = useState(false);
+
     return (<div>
-        <button>{title}</button>
-        <p>{description}</p>
+        {/* vado ad assegnare all'evento onClick la funzione setIsActive dove vado a cambiare lo stato iniziale dell'elemento false -> true */}
+        <button onClick={() => setIsActive(prev => !prev)}>{title}</button>
+
+        {/* di conseguenza, se lo stato iniziale è true, allora vedrò il p, altrimenti no */}
+        {isActive && <p>{description}</p>}
     </div>)
 
 }
