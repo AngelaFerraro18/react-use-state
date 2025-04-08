6f60af1883d33d9533b13a87abdc7a625b4867a7
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-08 16:54:13 +0200

    feat: change useState to open only one card by click

--- src/components/ButtonLanguage.jsx
+++ src/components/ButtonLanguage.jsx
@@ -1,20 +1,16 @@
-//importo useState da react
-import { useState } from "react";
+
 //importo il componente CardDescription
 import CardDescription from "./CardDescription";
 
-//creo il componente ButtonLanguage e come props vado a destrutturare per prendere direttamente gli elementi title e description
-function ButtonLanguage({ title, description }) {
-
-    //destrutturo e assegno dei nomi, dopodichè imposto lo stato iniziale a false
-    const [isActive, setIsActive] = useState(false);
+//creo il componente ButtonLanguage e come props vado a destrutturare per prendere direttamente gli elementi title e description, aggiungo le props create in ButtonList per tenere traccia della card aperta
+function ButtonLanguage({ title, description, isOpen, onToggle }) {
 
     return (<div>
         {/* vado ad assegnare all'evento onClick la funzione setIsActive dove vado a cambiare lo stato iniziale dell'elemento false -> true */}
-        <button onClick={() => setIsActive(prev => !prev)}>{title}</button>
+        <button onClick={onToggle}>{title}</button>
 
         {/* di conseguenza, se lo stato iniziale è true, allora vedrò il p, altrimenti no */}
-        {isActive && <CardDescription title={title} description={description} />}
+        {isOpen && <CardDescription title={title} description={description} />}
     </div>)
 
 }
--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,11 +1,16 @@
+//importo useState
+import { useState } from "react";
 //importo il componente ButtonLanguage
 import ButtonLanguage from "./ButtonLanguage";
 
 //creo il componente ButtonList e metto come props l'attributo languages e uso il metodo map per ricreare in pagina la lista degli elementi presenti nell'array fornito
 function ButtonList({ languages }) {
+
+    const [cardOpen, isCardOpen] = useState(null);
+
     return <div>
         {languages.map(element =>
-            <ButtonLanguage key={element.id} title={element.title} description={element.description} />
+            <ButtonLanguage key={element.id} title={element.title} description={element.description} isOpen={cardOpen === element.id} onToggle={() => isCardOpen(element.id)} />
         )}
     </div>
 }
